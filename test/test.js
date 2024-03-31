da51c2d1c9fe5be0301c592abdc9246f7c19cc46
Marcin Pawłowski <marcin.pawlowski1@microsoft.wsei.edu.pl>
2024-04-01 00:40:00 +0200

    Fixing edge test cases and changing name for node_modules to correct

--- test/test.js
+++ test/test.js
@@ -63,7 +63,7 @@ import('chai').then(module => {
 		it('should return false for an empty JSON object', () => {
 			const jsonData = {};
 			const result = verifyJSON(jsonData);
-			chai.expect(result).to.be.false;
+			expect(result).to.be.false;
 		});
 
 		it('should return false if PolicyDocument field is missing', () => {
@@ -71,7 +71,7 @@ import('chai').then(module => {
 				"PolicyName": "root"
 			};
 			const result = verifyJSON(jsonData);
-			chai.expect(result).to.be.false;
+			expect(result).to.be.false;
 		});
 
 		it('should return false if Statement array is empty', () => {
@@ -83,9 +83,116 @@ import('chai').then(module => {
 				}
 			};
 			const result = verifyJSON(jsonData);
-			chai.expect(result).to.be.false;
+			expect(result).to.be.false;
 		});
 
-		// Add more test cases for other edge cases
+		it('should return false if PolicyName field is missing', () => {
+			const jsonData = {
+				"PolicyDocument": {
+					"Version": "2012-10-17",
+					"Statement": [
+						{
+							"Sid": "IamListAccess",
+							"Effect": "Allow",
+							"Action": [
+								"iam:ListRoles",
+								"iam:ListUsers"
+							],
+							"Resource": "arn:aws:iam::123456789012:user/*"
+						}
+					]
+				}
+			};
+			const result = verifyJSON(jsonData);
+			expect(result).to.be.false;
+		});
+
+		it('should return false if the JSON data is not well-formed', () => {
+			const invalidData = '{"PolicyName": "root"';
+			const result = verifyJSON(invalidData);
+			expect(result).to.be.false;
+		});
+
+		it('should return true for valid JSON data with multiple statements', () => {
+			const jsonData = {
+				"PolicyName": "root",
+				"PolicyDocument": {
+					"Version": "2012-10-17",
+					"Statement": [
+						{
+							"Sid": "IamListAccess",
+							"Effect": "Allow",
+							"Action": [
+								"iam:ListRoles",
+								"iam:ListUsers"
+							],
+							"Resource": "arn:aws:iam::123456789012:user/*"
+						},
+						{
+							"Sid": "AnotherStatement",
+							"Effect": "Allow",
+							"Action": "s3:GetObject",
+							"Resource": "arn:aws:s3:::my-bucket/*"
+						}
+					]
+				}
+			};
+			const result = verifyJSON(jsonData);
+			expect(result).to.be.true;
+		});
+
+		it('should return true for valid JSON data with nested resources', () => {
+			const jsonData = {
+				"PolicyName": "root",
+				"PolicyDocument": {
+					"Version": "2012-10-17",
+					"Statement": [
+						{
+							"Sid": "IamListAccess",
+							"Effect": "Allow",
+							"Action": [
+								"iam:ListRoles",
+								"iam:ListUsers"
+							],
+							"Resource": {
+								"Fn::Join": [
+									":",
+									[
+										"arn",
+										"aws",
+										"iam",
+										"*"
+									]
+								]
+							}
+						}
+					]
+				}
+			};
+			const result = verifyJSON(jsonData);
+			expect(result).to.be.true;
+		});
+
+		it('should return false if the Resource field contains special characters other than an asterisk', () => {
+			const jsonData = {
+				"PolicyName": "root",
+				"PolicyDocument": {
+					"Version": "2012-10-17",
+					"Statement": [
+						{
+							"Sid": "IamListAccess",
+							"Effect": "Allow",
+							"Action": [
+								"iam:ListRoles",
+								"iam:ListUsers"
+							],
+							"Resource": "arn:aws:iam::123456789012:user/!@#$%^&*()"
+						}
+					]
+				}
+			};
+			const result = verifyJSON(jsonData);
+			expect(result).to.be.false;
+		});
 	});
 });
